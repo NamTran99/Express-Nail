b88d3d4df30ed74ff60ee0210a151fdc07358cab
ducdv <ducdang513@gmail.com>
2023-07-27 21:30:50 +0700

    Map API My Salon

--- app/src/main/java/com/example/nailexpress/views/ui/main/mysalon/MySalonFragment.kt
+++ app/src/main/java/com/example/nailexpress/views/ui/main/mysalon/MySalonFragment.kt
@@ -1,9 +1,12 @@
 package com.example.nailexpress.views.ui.main.mysalon
 
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.nailexpress.R
 import com.example.nailexpress.base.BaseFragment
 import com.example.nailexpress.databinding.FragmentMySalonBinding
+import com.example.nailexpress.models.ui.main.Salon
 import com.example.nailexpress.views.ui.main.mysalon.viewmodel.MySalonViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,5 +15,53 @@ class MySalonFragment : BaseFragment<FragmentMySalonBinding, MySalonViewModel>(R
     override val viewModel: MySalonViewModel by viewModels()
 
     override fun initView() {
+        with(binding) {
+            topBar.setOnBackPress {
+                findNavController().popBackStack()
+            }
+
+            viewModel.salonData.observe(viewLifecycleOwner) {
+                setUi(salon = it)
+            }
+
+        }
+    }
+
+    private fun setUi(salon: Salon?) {
+        if (salon == null) {
+            binding.layoutContent.isVisible = false
+        } else {
+            binding.layoutContent.isVisible = true
+            with(binding) {
+                shopInfoView.apply {
+                    shopName = salon.name
+                    phoneNumber = salon.phoneDisplay
+                    location = salon.address
+                }
+                salonActiveTime.apply {
+                    iilValue = salon.experience_years_display
+                }
+
+                majorityCustomerView.apply {
+                    iilValue = salon.skinColorDisplay
+                }
+
+                workerAccommodation.apply {
+                    iilValue = salon.display_have_place
+                }
+
+                shuttleBusWorker.apply {
+                    iilValue = salon.display_have_car
+                }
+
+                salonDescriptionView.apply {
+                    description = salon.description
+                }
+
+                salonPictureView.apply {
+                    setListPicture(salon.listImage)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/nailexpress/views/ui/main/mysalon/viewmodel/MySalonViewModel.kt
+++ app/src/main/java/com/example/nailexpress/views/ui/main/mysalon/viewmodel/MySalonViewModel.kt
@@ -1,10 +1,31 @@
 package com.example.nailexpress.views.ui.main.mysalon.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
 import com.example.nailexpress.base.BaseViewModel
+import com.example.nailexpress.extension.launch
+import com.example.nailexpress.models.ui.main.Salon
+import com.example.nailexpress.repository.SalonRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
-class MySalonViewModel @Inject constructor(application: Application) : BaseViewModel(application) {
+class MySalonViewModel @Inject constructor(
+    application: Application,
+    private val salonRepository: SalonRepository
+) : BaseViewModel(application) {
+
+    init {
+        getMySalon()
+    }
+
+    val salonData: MutableLiveData<Salon> = MutableLiveData()
+
+    private fun getMySalon() = launch {
+        salonRepository.getSalonDetail().onEach { salons ->
+            salonData.value = salons.getOrNull(0)
+        }.collect()
+    }
 }
