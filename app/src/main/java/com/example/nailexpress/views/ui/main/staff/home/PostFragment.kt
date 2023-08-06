0e299751c0f4d30b9a95de7068853f918b709f3a
NamTran <trandinhnam1199@gmail.com>
2023-08-06 17:59:55 +0700

    fix conflict

--- app/src/main/java/com/example/nailexpress/views/ui/main/staff/home/PostFragment.kt
+++ app/src/main/java/com/example/nailexpress/views/ui/main/staff/home/PostFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.nailexpress.base.PageRecyclerAdapter
 import com.example.nailexpress.databinding.FragmentPostBinding
 import com.example.nailexpress.views.ui.main.staff.HomeStaff
+import com.example.nailexpress.views.ui.main.staff.adapter.PostAdapter
 
-open class PostFragment : Fragment() {
+open class PostFragment(val adapter:  PageRecyclerAdapter<*,*>? = null) : Fragment() {
     lateinit var binding: FragmentPostBinding
         private set
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,6 +28,6 @@ open class PostFragment : Fragment() {
     }
 
     open fun setUp() {
-        binding.rvPost.adapter = (parentFragment as HomeStaff).viewModel.adapterPost
+        binding.rvPost.adapter = adapter ?: (parentFragment as HomeStaff).viewModel.adapterPost
     }
 }
---
+++ app/src/main/java/com/example/nailexpress/views/ui/nologin/NoLoginFragment.kt
@@ -0,0 +1,61 @@
+package com.example.nailexpress.views.ui.nologin
+
+import androidx.fragment.app.viewModels
+import com.example.nailexpress.R
+import com.example.nailexpress.base.BaseRefreshFragment
+import com.example.nailexpress.databinding.FragmentNoLoginBinding
+import com.example.nailexpress.views.ui.main.customer.nav_doash_board.NavAdapter
+import com.example.nailexpress.views.ui.main.staff.home.PostFragment
+import com.example.nailexpress.views.widgets.CustomHeaderHome
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NoLoginFragment :
+    BaseRefreshFragment<FragmentNoLoginBinding, NoLoginViewModel>(R.layout.fragment_no_login),
+    CustomHeaderHome.IActionHeader {
+    override val viewModel: NoLoginViewModel by viewModels()
+
+    override fun initView() {
+        binding.apply {
+            vm = viewModel
+            with(vpMain) {
+                if (adapter == null) {
+                    adapter = NavAdapter(
+                        this@NoLoginFragment, listOf(
+                            PostFragment(viewModel.adapterPost),
+                            PostFragment(viewModel.adapterNailStaff)
+                        )
+                    )
+                }
+                isSaveEnabled = true
+                isUserInputEnabled = false
+
+                viewModel.isTabPost.observe(viewLifecycleOwner) {
+                    currentItem = if (it) TAB_POST else TAB_CV
+                }
+            }
+
+            with(header) {
+                updateAction(this@NoLoginFragment)
+                updateTextNotification("")
+            }
+        }
+    }
+
+    override fun onTextChange(string: String) {
+
+    }
+
+    override fun onClickNotification() {
+        //không có notification tại màn này
+    }
+
+    override fun onClickFilter() {
+
+    }
+
+    companion object {
+        private const val TAB_POST = 0
+        private const val TAB_CV = 1
+    }
+}
